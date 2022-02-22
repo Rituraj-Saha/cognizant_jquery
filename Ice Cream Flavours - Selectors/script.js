4ec38d119c664eb7e9c8f9cb1f1e52a36fd22248
Rituraj-Saha <61684573+Rituraj-Saha@users.noreply.github.com>
2022-02-22 21:56:02 +0530

    Create script.js

---
+++ Ice Cream Flavours - Selectors/script.js
@@ -0,0 +1,6 @@
+// WRITE YOUR CODE HERE
+// WRITE YOUR CODE HERE
+$("p").css({"color":"orange"});
+$("p:nth-child(2)").css({"font-size":"50%"});
+$(".flavours  li").css({"background-color":"lightblue","font-size":"120%"});
+$("#icecreamfloats li").css({"background-color":"orange"});
